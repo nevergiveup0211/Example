4f76890d2a1dc3e16f5db7503f1d5ffa4b9e2b86
nevergiveup0211 <nevergiveup0211@gmail.com>
2019-04-08 21:28:17 +0530

    committed

---
+++ src/main/java/chiranjeevi/App.java
@@ -0,0 +1,13 @@
+package chiranjeevi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello friend!" );
+    }
+}
